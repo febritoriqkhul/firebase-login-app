807bca0d14d5d8966985b6818612c0f63af1ad0f
febritoriqkhul <febritoriqkhul@gmail.com>
2020-04-23 11:50:15 +0700

    Add file firebase.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBApUXsz_fgzTbI7gxnBX9F4N76SUt57NA",
+    authDomain: "fir-login-540c8.firebaseapp.com",
+    databaseURL: "https://fir-login-540c8.firebaseio.com",
+    projectId: "fir-login-540c8",
+    storageBucket: "fir-login-540c8.appspot.com",
+    messagingSenderId: "62666151091",
+    appId: "1:62666151091:web:40da63f1dbfd521832ccc6",
+    measurementId: "G-T6R7WCB2C3"
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
